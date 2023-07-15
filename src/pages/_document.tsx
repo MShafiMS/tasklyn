c31410b41c2c93fce8214a1b061d2f2b2dcd2d0d
MShafiMS <shahedali734@gmail.com>
2023-07-15 22:09:05 +0600

    updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,25 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
       <title>Tasklyn</title>
+      <Head>
+        <meta property="og:title" content="Tasklyn" />
+        <meta property="og:site_name" content="Tasklyn" />
+        <meta
+          property="og:url"
+          content="https://main.d3188mhqzkjmhs.amplifyapp.com/todo"
+        />
+        <meta
+          property="og:description"
+          content="This is a simple Todo site built using Next.js, Tailwind CSS, Mobx State Tree, and AWS Amplify GraphQL API."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/BwfMj1P/Group-6.png"
+        ></meta>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
