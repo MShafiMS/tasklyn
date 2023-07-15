cfdd46584b2130dfd6c2ef5187695b4e758e35a6
MShafiMS <shahedali734@gmail.com>
2023-07-15 14:37:33 +0600

    configured hosting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
+    unoptimized: true,
     domains: ["webb-school-mission-2022.vercel.app"],
   },
 };
